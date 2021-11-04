81f0a502f97831f5f2b50c0492d4c72e070b6943
Paul-Émile AILLAUD <aillaud.pe@gmail.com>
2021-11-04 11:30:41 +0100

    Amélioration bouton accueil

--- scripts/accueil.js
+++ scripts/accueil.js
@@ -1,5 +1,17 @@
-var button = document.querySelector('body > div.accueil > div.autoScroll > i')
+var button = document.querySelector('body > div.accueil > div.autoScroll')
 
 button.onclick = function() {
-    location.href = "#contenu"
+        location.href = "#contenu";
+    }
+    /**
+     * Gère l'afffichage du bouton en fonction de ce qu'a descendu l'utilisateur sur la page    
+     */
+function verifBouton() {
+    if (getComputedStyle(button).opacity == '1' && document.documentElement.scrollTop > 80) {
+        button.style.opacity = '0';
+    } else if (getComputedStyle(button).opacity == '0' && document.documentElement.scrollTop == 0) {
+        button.style.opacity = '1';
+    }
 }
+
+window.onscroll = verifBouton;
