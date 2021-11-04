83ef70b51e046837610f78a5a25083a999b8aa65
Paul-Émile AILLAUD <aillaud.pe@gmail.com>
2021-11-04 14:27:52 +0100

    Images

--- scripts/accueil.js
+++ scripts/accueil.js
@@ -1,11 +1,12 @@
 var button = document.querySelector('body > div.accueil > div.autoScroll')
 
 button.onclick = function() {
-        location.href = "#contenu";
-    }
-    /**
-     * Gère l'afffichage du bouton en fonction de ce qu'a descendu l'utilisateur sur la page    
-     */
+    location.href = "#contenu";
+}
+
+/**
+ * Gère l'afffichage du bouton en fonction de ce qu'a descendu l'utilisateur sur la page    
+ */
 function verifBouton() {
     if (getComputedStyle(button).opacity == '1' && document.documentElement.scrollTop > 80) {
         button.style.opacity = '0';
