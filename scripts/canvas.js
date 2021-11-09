54b8660c009f470588ef0713dd53cef891a8fe51
Simon GREGOIRE <grego-sim@saint-joseph-gap.com>
2021-11-09 09:11:13 +0100

    webgl hell

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,3 +1,23 @@
+function loadShader(gl, type, source) {
+    const shader = gl.createShader(type);
+
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        return null;
+    } else {
+        return shader;
+    }
+}
+
+function createShaderProgram(gl, vSource, fSource) {
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vSource);
+}
+
 window.onload = function() {
     /** @type {HTMLCanvasElement} */
     const canvas = document.querySelector("#canvas1");
@@ -9,8 +29,6 @@ window.onload = function() {
         alert("webgl non supporté");
         return;
     }
-
-    const vertexShader = 
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
