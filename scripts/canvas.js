9acef876deeaa2f8a99c68cc93398e685159aea9
Simon GREGOIRE <grego-sim@saint-joseph-gap.com>
2021-11-09 09:40:08 +0100

    webgl hell

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,9 +1,38 @@
+window.addEventListener('load', function () {
+    /** @type {HTMLCanvasElement} */
+    const canvas = document.querySelector("#canvas1");
+
+    /** @type {WebGLRenderingContext} */
+    const gl = canvas.getContext("webgl");
+
+    if (!gl) {
+        alert("webgl non supporté");
+        return;
+    }
+
+    this.fetch('../shaders/vertex.glsl').then(x => x.text()).then(y => alert(y));
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    const vbo = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+
+    const positions = [
+        1.0, 1.0,
+        -1.0, 1.0,
+        1.0, -1.0,
+        -1.0, -1.0,
+    ];
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+})
+
 function loadShader(gl, type, source) {
     const shader = gl.createShader(type);
 
     gl.shaderSource(shader, source);
     gl.compileShader(shader);
-
 
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
         alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
@@ -16,32 +45,18 @@ function loadShader(gl, type, source) {
 
 function createShaderProgram(gl, vSource, fSource) {
     const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vSource);
-}
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fSource);
 
-window.addEventListener('load', function() {
-    /** @type {HTMLCanvasElement} */
-    const canvas = document.querySelector("#canvas1");
+    const shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
 
-    /** @type {WebGLRenderingContext} */
-    const gl = canvas.getContext("webgl");
+    gl.linkProgram(shaderProgram);
 
-    if (!gl) {
-        alert("webgl non supporté");
-        return;
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert("Impossible d'initialiser le programme shader : " + gl.getProgramInfoLog(shaderProgram));
+        return null;
+    } else {
+        return shaderProgram;
     }
-
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
-
-    const vbo = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-
-     const positions = [
-        1.0,  1.0,
-        -1.0,  1.0,
-        1.0, -1.0,
-        -1.0, -1.0,
-    ];
-
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-})
+}
