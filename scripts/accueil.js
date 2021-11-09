e25ee5f6bdc9c8e112135d85c0b851bd974f2fb8
peaillaud <aillaud.pe@gmail.com>
2021-11-09 09:41:41 +0100

    Canvas

--- scripts/accueil.js
+++ scripts/accueil.js
@@ -1,6 +1,7 @@
 var button = document.querySelector('body > div.accueil > div.autoScroll');
 var suiteSite = document.getElementsByClassName('suiteSite')[0];
 var donneeY = suiteSite.getBoundingClientRect().top + window.pageYOffset;
+var quotes = document.querySelectorAll('.quote');
 
 /**
  * Gère l'afffichage du bouton de bienvenue en fonction de ce qu'a descendu l'utilisateur sur la page    
@@ -13,7 +14,11 @@ function verifBouton() {
     }
 }
 
-// Convertir les VH en PX pour compenser la taille du header lors du scroll
+/**
+ * 
+ * @param {float} valeur Valeur en vh qui va être convertie en px
+ * @returns {float} Retourne la valeur en px
+ */
 
 function vhPx(valeur) {
     var y = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
@@ -22,10 +27,12 @@ function vhPx(valeur) {
     return result;
 }
 
+window.onscroll = function() {
+    verifBouton();
+}
 
-window.onscroll = verifBouton;
 button.onclick = function() {
     window.scrollTo({
-        top: donneeY - vhPx(10)
+        top: donneeY - vhPx(15)
     })
 }
